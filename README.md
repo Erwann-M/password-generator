bd515d06afb64cac2556f1c598971e6e81a2876e
Erwann-M <rwnnmrtn@gmail.com>
2022-04-14 11:36:31 +0200

    add copy system

---
+++ README.md
@@ -0,0 +1,19 @@
+# Password Generator
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+A simple tool for generate strong password
+
+![screenshot](assets/screenshot.png)
+
+## installation
+
+- install tkinter -> ```pip install tk```
+- install pyperclip -> ```pip install pyperclip```
+
+On Linux install one of the copy/paste mechanisms:
+
+- ```sudo apt-get install xsel``` to install the xsel utility.
+- ```sudo apt-get install xclip``` to install the xclip utility.
+- ```pip install gtk``` to install the gtk Python module.
+- ```pip install PyQt4``` to install the PyQt4 Python module.
--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from random import randint
+from functools import partial
 from secrets import choice
+import pyperclip
 import string
 from tkinter import *
 
-
-
-number_chars = 2
-
-def generate_password():
-    all_chars = string.ascii_letters + string.punctuation + string.digits
+class GeneratorSystem:
     
-    password = "".join(choice(all_chars) for x in range(number_chars))
-    password_entry.delete(0, END)
-    password_entry.insert(0, password)
+    def __init__(self):
+        self.number_chars = 2
+
+    def generate_password(self):
+        all_chars = string.ascii_letters + string.punctuation + string.digits
+        
+        self.password = "".join(choice(all_chars) for x in range(int(self.number_chars)))
+        password_entry.delete(0, END)
+        password_entry.insert(0, self.password)
+        
+    def change_number_chars(self, number):
+        self.number_chars = number
+        label_number_chars.config(text="de: " + str(self.number_chars) + " caractères")
+        
+    def copy_password(self):
+        pyperclip.copy(self.password)
+        
+    def get_number_chars(self):
+        return self.number_chars
     
 
 backgroud_color = "#83a2ad"
+newPassword = GeneratorSystem()
+
 
 # Création de la fenetre
 window = Tk()
@@ -23,32 +37,47 @@ window.title("Générateur de mot de passe")
 window.geometry("720x480")
 window.config(background=backgroud_color)
 
+
 # Création de la frame principale
 frame = Frame(window, bg=backgroud_color)
+
 
 #creation d'une image
 width = 300
 height = 300
-image = PhotoImage(file="password-image.png")
+image = PhotoImage(file="assets/password-image.png")
 canvas = Canvas(frame, width=width, height=height, bg=backgroud_color, bd=0, highlightthickness=0)
 canvas.create_image(width/2, height/2, image=image)
 canvas.grid(row=0, column=0, sticky=W)
 
+
 # Création d'une sous frame
 right_frame = Frame(frame, bg=backgroud_color)
+
 
 # Créer un titre
 label_title = Label(right_frame, text="Mot de passe", font=("Helvetica", 20), bg=backgroud_color, fg="white")
 label_title.pack()
 
+
+# Information du nombre de caractère
+label_number_chars = Label(right_frame, text="de: " + str(newPassword.get_number_chars()) + " caractères", font=("Helvetica", 20), bg=backgroud_color, fg="white")
+label_number_chars.pack()
+
+
 # Créer un input
 password_entry = Entry(right_frame, font=("Helvetica", 15), bg=backgroud_color, fg="white", bd=1, highlightthickness=0)
 password_entry.pack(pady=10)
 
-# Créer le bouton de génération de mot de passe
-generate_button = Button(right_frame,  text="Générer", font=("Helvetica", 15), bg="white", fg=backgroud_color, bd=1, highlightthickness=0, command=generate_password)
 
-generate_button.pack(fill=X)
+# Créer le bouton de génération de mot de passe
+generate_button = Button(right_frame,  text="Générer", font=("Helvetica", 15), bg="white", fg=backgroud_color, bd=1, highlightthickness=0, command=newPassword.generate_password)
+generate_button.pack(fill=X, pady=10)
+
+# Bouton pour copier le mot de passe
+copy_button = Button(right_frame,  text="Copier le MDP", font=("Helvetica", 15), bg="white", fg=backgroud_color, bd=1, highlightthickness=0, command=newPassword.copy_password)
+copy_button.pack(fill=X)
+
 
 right_frame.grid(row=0, column=1, sticky=W)
 
@@ -59,10 +88,18 @@ menu_bar = Menu(window)
 
 # Création d'un premier menu
 file_menu  = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Nouveau", command=generate_password)
+file_menu.add_command(label="Nouveau", command=newPassword.generate_password)
 file_menu.add_command(label="Quitter", command=window.quit)
 
 menu_bar.add_cascade(label="Fichier", menu=file_menu)
+
+# Création du menu de choix du nombre de caractere
+select_number_menu = Menu(menu_bar, tearoff=0)
+for x in range(6, 21):
+    
+    select_number_menu.add_command(label=x, command=partial(newPassword.change_number_chars, x))
+
+menu_bar.add_cascade(label="Nombre de caractère", menu=select_number_menu)
 
 # Ajouter le menu a la fenetre
 window.config(menu=menu_bar)
